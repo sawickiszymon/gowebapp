89254cbb5a3d84cc5ac9d16d4e2bddd6d6889265
Szymon <sz.sawicki1@gmail.com>
2020-08-17 17:03:45 +0200

    SendMessagesv5

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	router.POST("/api/message", handler.PostMessage)
 	router.GET("/api/message/:email", handler.ViewMessages)
 	router.POST("/api/send", handlers.SendMessages(s))
+	router.POST("/api/sendx", handler.Test)
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
